d41bfa0cba127827c13b9b2e7de15e08ae78e41a
Eduardo Perez Motato <1709992@uab.cat>
2024-09-28 23:01:14 +0200

    Fet tot per avui, bastant bé

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -30,37 +30,54 @@ export const modalitatsOptions = [
 export const roadmapItems = [
     {
         description: "Creació logo matc.ad (.svg).",
-        date: "01/01/2023"
+        date: "29/09/2024"
     },
     {
-        description: "Fer més bonica la pàgina web.",
-        date: "15/02/2022"
+        description: "Conseguim matc.ad.",
+        date: "16/10/2024"
     },
     {
-        description: "Creació d'un roadmap correcte amb les coses que volem fer després de tot això.",
-        date: "30/03/2023"
+        description: "Creació subodminis facils (peix, transparencia) administraciò.",
+        date: "4/10/2024"
     },
     {
-        description: "Creaciò de la pàgina de preus.",
-        date: "1/04/2024"
+        description: "Inici de peix@matc.ad.",
+        date: "25/10/2024"
+    },
+    {
+        description: "Creació mailing system.",
+        date: "23/10/2024"
+    },
+    {
+        description: "Inici de newslater@matc.ad.",
+        date: "25/10/2024"
+    },
+    {
+        description: "Inici editor.matc.ad.",
+        date: "16/11/2024"
+    },
+    {
+        description: "Inici git.matc.ad.",
+        date: "13/12/2024"
     }
 ];
 
 export const formulariContacte = "https://matc.ad/formulari";
 
 export const preguntesFreq = [
-    { question: "Per què?", answer: "Som un grup d'estudiants que ens va fer gràcia poder tenir el correu @matc.ad. Fa poc Andorra va començar a permetre la compra dels .ad i per això ho hem comprat. Hem decidit crear aquesta pàgina web i més serveis que ens seran útils o divertits." },
-    { question: "Pregunta 2", answer: "Resposta a la pregunta 2." },
-    { question: "Pregunta 3", answer: "Resposta a la pregunta 3." },
+    { question: "Per què heu fet aquesta pàgina web?", answer: "Som un grup d'estudiants que ens va fer gràcia poder tenir el correu @matc.ad. Fa poc Andorra va començar a permetre la compra dels .ad i per això ho hem comprat. Hem decidit crear aquesta pàgina web i més serveis que ens seran útils o divertits." },
+    { question: "Com em creo un compte?", answer: "Molt fàcil, dona-li a \"formulari de contacte\" i pots fer una petició." },
+    { question: "Què més fa aquesta pàgina web a part de demanar diners?", answer: "Pot redirigir altres pàgines com un acurtador d'enllaços. Prova matc.ad/song (nota: ara mateix no tenim matc.ad però es pot fer amb matcad.pages.dev/song)." },
+    { question: "Puc demanar una redirecció?", answer: "Sí. Dona-li al formulari de contacte i allà pots enviar una petició." },
 ];
 
 export const navLinks = [
     { label: 'Modalitats', path: 'modalitats' },
     { label: 'Subdominis', path: 'subdomains' },
     { label: 'Preguntes Freqüents', path: 'faq' },
-    { label: 'Roadmap', path: 'roadmap' },
-    { label: 'Formulari de contacte', href: formulariContacte},
-    { label: 'Redireccions', href: '/redireccions' }
+    { label: 'Full de ruta', path: 'roadmap' },
+    { label: 'Redireccions', href: '/redireccions' },
+    { label: 'Formulari de contacte', href: formulariContacte}
 ];
 
 export const colorsOptions = {
@@ -89,15 +106,14 @@ export const colorsOptions = {
 };
 
 export const subdomainsOptions = [
-    { title: "www", description: "Efectivament. www no ès mort, ja que mostra aquesta pàgina web. Apart d'això, també fa de acurtador d'enllaços utils per la comunitat. Vols saber totes? Fes https://matc.ad/redireccions (nota: encara no tenim matc.ad pero pots fer https://matcad.pages.dev/redireccions)", url: "https://matc.ad" },
-    { title: "repster", description: "Pagina de RepSter.", url: "https://repster.matc.ad" },
+    { title: "transparencia", description: "Transparència de la nostra organització.", url: "https://transparencia.matc.ad"},
     { title: "peix", description: "Acudits de peixos. Res més.", url: "https://peix.matc.ad"},
+    { title: "repster", description: "Pagina de RepSter.", url: "https://repster.matc.ad" }
 ];
 
 export const redireccions = [
-    { page: "repster", url: "https://repster.matc.ad"},
-    { page: "peix", url: "https://peix.matc.ad"},
     { page: "apunts", url: "https://github.com/TheRepSter/Apunts-MatCAD"},
     { page: "song", url: "https://youtu.be/dQw4w9WgXcQ"},
-    { page: "formulari", url: "https://youtu.be/dQw4w9WgXcQ"}
+    { page: "formulari", url: "https://youtu.be/dQw4w9WgXcQ"},
+    ...subdomainsOptions.map(subdomain => ({ page: subdomain.title, url: subdomain.url }))
 ];
